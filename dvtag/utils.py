bca819534fa5946b45c78e4d83fc850389c30627
NobeKanai <qidada4880@gmail.com>
2021-09-28 12:06:08 +0800

    feat: allow multi disc in single folder

--- dvtag/utils.py
+++ dvtag/utils.py
@@ -13,6 +13,48 @@ from PIL import Image
 re_rjid = re.compile(r"RJ[0-9]{6}", flags=re.IGNORECASE)
 
 
+def _split(audio_files: List[Path]) -> List[List[Path]]:
+    regexes = [
+        r'^omake_?.*[0-9]{1,2}.*$',
+        r'^.*ex[0-9]{1,2}.*$',
+        r'^ex_.+$',
+        r'^後日談.*$',
+        r'^おまけ_?[0-9]{0,2}.*$',
+        r'^反転おまけ_?[0-9]{1,2}.*$',
+        r'^反転_?[0-9]{1,2}.*$',
+        r'^20..年?[0-9]{1,2}月配信.*$',
+        r'^.*特典.*$',
+        r'^追加[0-9]{1,2}.*$',
+        r'^opt[0-9]?.*',
+        r'^#[0-9]+(-|ー)B',
+        r'^#[0-9]+(-|ー)C',
+        r'^ASMR_.*',
+        r'^.+Bパート',
+        r'^番外編',
+    ]  # regex expressions must keep no collision with each other
+
+    results = {}
+    paths = [[]]
+    for audio_file in audio_files:
+        matched = False
+        for regex_expr in regexes:
+            if re.match(regex_expr, audio_file.stem, re.IGNORECASE):
+                matched = True
+                if results.get(regex_expr):
+                    results[regex_expr].append(audio_file)
+                else:
+                    results[regex_expr] = [audio_file]
+                break
+
+        if not matched:
+            paths[0].append(audio_file)
+
+    for _, v in results.items():
+        paths.append(v)
+
+    return paths
+
+
 def get_audio_files(
         basepath: Path) -> Tuple[List[List[Path]], List[List[Path]]]:
     """get audio files(Path) from base_path recursively
@@ -36,9 +78,9 @@ def get_audio_files(
                 mp3_files.append(Path(os.path.join(dirpath, filename)))
 
         if len(flac_files):
-            flac_file_lists.append(flac_files)
+            flac_file_lists.extend(_split(flac_files))
         if len(mp3_files):
-            mp3_file_lists.append(mp3_files)
+            mp3_file_lists.extend(_split(mp3_files))
 
     return flac_file_lists, mp3_file_lists
 
